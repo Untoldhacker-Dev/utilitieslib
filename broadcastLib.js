6d57c21a68aa94a72afa15a716faac0b5c355142
Untoldhacker-Dev <74806602+Untoldhacker-Dev@users.noreply.github.com>
2021-06-26 11:56:08 +0530

    Update broadcastLib.js

--- broadcastLib.js
+++ broadcastLib.js
@@ -1,1 +1,9 @@
-
+function broadcast(text){
+  Bot.runAll({command:"&&XC-Broadcast"})
+  Bot.setProperty("recent_broadcast",text)
+  on('&&XC-Broadcast', Bot.sendMessage("Broadcast!\n\n"+Bot.getProperty("recent_broadcast"))
+     
+     }
+publish({
+     broadcast: broadcast
+     })
