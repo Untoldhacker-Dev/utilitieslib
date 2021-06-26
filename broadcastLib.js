1576bac07b5cc9960fcf9d9984e8a4fee3408800
Untoldhacker-Dev <74806602+Untoldhacker-Dev@users.noreply.github.com>
2021-06-26 12:01:15 +0530

    Fixed error: missing "}"

--- broadcastLib.js
+++ broadcastLib.js
@@ -1,9 +1,13 @@
-function broadcast(text){
-  Bot.runAll({command:"&&XC-Broadcast"})
-  Bot.setProperty("recent_broadcast",text)
-  on('&&XC-Broadcast', Bot.sendMessage("Broadcast!\n\n"+Bot.getProperty("recent_broadcast"))
-     
-     }
-publish({
-     broadcast: broadcast
-     })
+function broadcast(text) {
+
+  Bot.runAll({ command: "&&XC-Broadcast" })
+
+  Bot.setProperty("recent_broadcast", text)
+
+  var msg = Bot.getProperty("recent_broadcast")
+
+  on("&&XC-Broadcast", Bot.sendMessage("Broadcast!\n\n" + msg))
+
+}
+
+publish({ broadcast: broadcast })
